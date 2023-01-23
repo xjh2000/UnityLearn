a448a95541719ef6f2a37247301385126ba1a5fa
xjh <934622645@qq.com>
2023-01-23 15:45:39 +0800

    + powerUp prop + powerUpIndicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,59 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     private Rigidbody _playerRb;
-    private GameObject focalPoint;
-
-
+    private GameObject _focalPoint;
+    private bool _hasPowerup;
+    private float _powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
+        _focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
-        _playerRb.AddForce(focalPoint.transform.forward * (speed * verticalInput));
+        _playerRb.AddForce(_focalPoint.transform.forward * (speed * verticalInput));
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            _hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        _hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && _hasPowerup)
+        {
+            var rb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            rb.AddForce(awayFromPlayer * _powerupStrength, ForceMode.Impulse);
+        }
     }
 }
